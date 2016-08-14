45bb06683b2037dd3e90abd593aac6b414ccc507
Mehul Mali <Mehul Mali>
2016-08-14 20:51:48 +0530

    added local.js

---
+++ config/env/local.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Rename this file to local.js for having a local configuration variables that
+// will not get commited and pushed to remote repositories.
+// Use it for your API keys, passwords, etc.
+
+//For example:
+
+module.exports = {
+    db: {
+        uri: 'mongodb://meds:meds@ds153715.mlab.com:53715/meds',
+        options: {
+            user: 'meds',
+            pass: ''
+        }
+    },
+    google: {
+        clientID: process.env.GOOGLE_ID || '619369132177-naovs16pij0lirklvbc06gi8fbgeb4lu.apps.googleusercontent.com',
+        clientSecret: process.env.GOOGLE_SECRET || 'DaYOKza68Rl52i20YEAflLqr',
+        callbackURL: 'https://nirmal-solanki.herokuapp.com/api/auth/google/callback'
+    }
+};
